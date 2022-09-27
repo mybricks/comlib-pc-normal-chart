bbbead341d9a99202235295d68f1c800ccfd23f7
z35635 <linzhujlu@foxmail.com>
2022-09-27 15:59:56 +0800

    ref: area

--- src/area/runtime.tsx
+++ src/area/runtime.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Area } from '@ant-design/charts';
-import BaseChart from '../components/chart';
-import { Area as MockData } from '../mock';
+import { Data, InputIds, MockData } from './constants';
 
-export default function(args) {
+export default function ({ data, env, inputs, style }: RuntimeParams<Data>) {
+  const [dataSourceInRuntime, setRuntimeDataSource] = useState([]);
 
-  return <BaseChart {...args} Chart={Area} MockData={MockData}/>
+  useEffect(() => {
+    if (env.runtime) {
+      inputs[InputIds.SetData]((val) => {
+        setRuntimeDataSource(val);
+      });
+    }
+  }, []);
+
+  return (
+    <Area
+      {...style}
+      {...data.config}
+      data={env.edit ? MockData[data.subType] : dataSourceInRuntime}
+      key={env.edit ? JSON.stringify(data.config) : undefined}
+    />
+  );
 }
