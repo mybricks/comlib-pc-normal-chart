36b56855b7998604832bf15e9fc0262ccb733911
zhangzhenfeng <zhangzhenfeng@kuaishou.com>
2022-09-27 17:27:17 +0800

    feat: remove useless file

--- src/heatmap/const.ts
+++
@@ -1,6 +0,0 @@
-export const DISPLAY_LABEL = Symbol('display_label');
-
-export enum DataOrigin {
-  'main',
-  'sub'
-}
--- src/organization/runtime.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { OrganizationGraph }from '@ant-design/graphs';
-import BaseChart from '../components/chart';
-import { Organization as MockData } from '../mock';
-
-export default function(args) {
-  return <BaseChart {...args} Chart={OrganizationGraph} MockData={MockData}/>
-}
