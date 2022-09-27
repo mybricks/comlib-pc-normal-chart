bc9f6d0f913b58fab0491ec51b18f08b127e4678
z35635 <linzhujlu@foxmail.com>
2022-09-27 14:53:10 +0800

    ref: line

---
+++ src/line/editor/baseEditor.ts
@@ -0,0 +1,54 @@
+import { Data, InputIds, Schemas, SubTypeEnum } from '../constants';
+
+const BaseEditor = [
+  {
+    title: '类型',
+    type: 'Select',
+    options: [
+      { label: '基础折线图', value: 'default' },
+      { label: '多折线图', value: 'more' },
+      { label: '阶梯折线图', value: 'step' },
+    ],
+    value: {
+      get({ data }: EditorResult<Data>) {
+        return data.subType;
+      },
+      set({ data, input }: EditorResult<Data>, value: SubTypeEnum) {
+        data.subType = value;
+        const dsInput = input.get(InputIds.SetData);
+        switch (value) {
+          case SubTypeEnum.Step:
+            data.config.stepType = 'vh';
+            data.config.seriesField = '';
+            dsInput.setSchema(Schemas.DefaultDataSource);
+            break;
+          case SubTypeEnum.More:
+            data.config.stepType = '';
+            data.config.seriesField = 'seriesField';
+            dsInput.setSchema(Schemas.MoreDataSource);
+            break;
+          default:
+            data.config.stepType = '';
+            data.config.seriesField = '';
+            dsInput.setSchema(Schemas.DefaultDataSource);
+        }
+      },
+    },
+  },
+  {
+    title: '平滑曲线',
+    ifVisible({ data }) {
+      return data.subType !== SubTypeEnum.Step;
+    },
+    type: 'switch',
+    value: {
+      get({ data }: EditorResult<Data>) {
+        return data.config.smooth;
+      },
+      set({ data }: EditorResult<Data>, value: boolean) {
+        data.config.smooth = value;
+      },
+    },
+  },
+];
+export default BaseEditor;
