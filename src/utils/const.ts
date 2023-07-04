6d055657aefcc018fd5cf4b537f61f28ee8659cf
zhuba-Ahhh <3477826311@qq.com>
2023-07-04 16:29:43 +0800

    feat(changeV2): area

---
+++ src/utils/const.ts
@@ -0,0 +1,93 @@
+import { Annotation } from '@antv/g2plot';
+
+export const colors10 = [
+  '#5B8FF9',
+  '#5AD8A6',
+  '#5D7092',
+  '#F6BD16',
+  '#6F5EF9',
+  '#6DC8EC',
+  '#945FB9',
+  '#FF9845',
+  '#1E9493',
+  '#FF99C3',
+];
+
+export const colors20 = [
+  '#5B8FF9',
+  '#CDDDFD',
+  '#5AD8A6',
+  '#CDF3E4',
+  '#5D7092',
+  '#CED4DE',
+  '#F6BD16',
+  '#FCEBB9',
+  '#6F5EF9',
+  '#D3CEFD',
+  '#6DC8EC',
+  '#D3EEF9',
+  '#945FB9',
+  '#DECFEA',
+  '#FF9845',
+  '#FFE0C7',
+  '#1E9493',
+  '#BBDEDE',
+  '#FF99C3',
+  '#FFE0ED',
+];
+
+export enum ChartType {
+  'Area' = 'Area',
+  'Bar' = 'Bar',
+  'Line' = 'Line',
+  'Pie' = 'Pie',
+  'Column' = 'Column',
+  'Liquid' = 'Liquid',
+  'Radar' = 'Radar',
+  'Funnel' = 'Funnel',
+  'BidirectionalBar' = 'BidirectionalBar',
+  'Heatmap' = 'Heatmap',
+  'TinyColumn' = 'TinyColumn',
+  'TinyArea' = 'TinyArea',
+  'RingProgress' = 'RingProgress',
+  'DualAxes' = 'DualAxes',
+  'OrganizationGraph' = 'OrganizationGraph'
+}
+
+/**
+ * @param label 标注的名称
+ * @param yFieldIndex 图表为双轴图时，标注的y轴字段索引
+ * @param positionField 文本/数据点动态设置坐标的字段
+ * @param textColor 文本颜色
+ * @param mainColor 标注的主体颜色, 如辅助线的线段颜色
+ * @param useDash 辅助线标注是否使用虚线
+ * @param autoRotate 文本是否自动旋转
+ */
+export declare type AnnotationItem = {
+  label?: string;
+  yFieldIndex?: 0 | 1;
+  positionField?: string;
+  textColor?: string;
+  mainColor?: string;
+  useDash?: boolean;
+  autoRotate?: boolean;
+} & Annotation;
+
+export const InputIds = {
+  DataSource: 'data',
+}
+
+export const DefaultDualGeometryOptions = [
+  {
+    geometry: 'line',
+    color: '#5AD8A6',
+  },
+  {
+    geometry: 'line',
+    color: '#5B8FF9',
+  },
+];
+
+export interface Data {
+  [key: string]: any;
+}
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,5 @@
+import { ChartType, Data } from './const';
+
 export enum AxisPositionEnum {
   Start = 'start',
   Center = 'center',
@@ -39,4 +41,71 @@ export interface ChartProps {
   config: ChartConfigProps;
   useEmpty?: boolean;
   emptyText?: string;
+}
+
+export const initInput = (type: string) => {
+  switch (type) {
+    default:
+      return [
+        {
+          id: 'style',
+          title: '设置图形样式',
+        },
+        { id: 'axis', title: '设置横轴' },
+        { id: 'yaxis', title: '设置纵轴' },
+        { id: 'label', title: '设置label' },
+        { id: 'tooltip', title: '设置提示信息' },
+        { id: 'legend', title: '设置图例' },
+        { id: 'loading', title: '设置加载态', schema: { type: 'boolean' } },
+      ];
+  }
+};
+
+export const initEvents = ({ data, input, output }) => {
+  if (data.type === ChartType.OrganizationGraph) {
+    output.add('nodeClick', '节点点击', { type: 'any' });
+  }
+}
+
+export const schemaDiff = (data: Data) => {
+  return {
+    title: '输入数据',
+    type: 'array',
+    items:{
+    type: 'object',
+      properties: {
+        [data.config.xField]: {
+          title: 'x轴字段名',
+          type: 'string'
+        },
+        [data.config.yField]: {
+          title: 'y轴字段名',
+          type: 'number'
+        },
+        [data.config.seriesField]: {
+          title: '分组字段名',
+          type: 'string'
+        }
+      }
+    }
+  }
+}
+
+export const reRender = (data) => (data.config = { ...data.config });
+
+export const inputIdMap = {
+  DATA: 'data', // 数据源
+  CONFIG: 'config', // 图表配置
+  X_FIELD: 'xField', // x轴配置
+  Y_FIELD: 'yField', // y轴配置
+  COLOR_FIELD: 'colorField', // 维度字段名
+  ANGLE_FIELD: 'angleField', // 数据字段名
+  SERIES_FIELD: 'seriesField', // 分组字段名
+  LEGEND: 'legend',
+  LEGEND_SELECTED: 'legend.selected',
+  LEGEND_POSITION: 'legend.position',
+  SMOOTH: 'smooth', // 是否平滑
+  // TOOLTIP_FORMATTER: 'tooltip.formatter',
+  LABEL: 'label', // 数据标签
+  TOOLTIP: 'tooltip' // 悬浮提示
 }
