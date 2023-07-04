af4cd5a0769fc8d4458c32782be6c5a9b21146e1
zhuba-Ahhh <3477826311@qq.com>
2023-07-04 22:19:36 +0800

    feat(changeV2): ringProgress

--- src/ringProgress/constants.ts
+++ src/ringProgress/constants.ts
@@ -1,26 +1,25 @@
-import { ChartConfigProps } from '../utils/constants';
-
-type StatisticText =
-  | boolean
-  | {
-      content?: string;
-    };
-
-export interface Data {
-  config: ChartConfigProps & {
-    percent?: number;
-    color?: [string, string];
-    innerRadius?: number;
-    radius?: number;
-    statistic?: {
-      title?: StatisticText;
-      content?: StatisticText;
-    };
-  };
-}
-
 export const InputIds = {
   SetData: 'data'
 };
 
-export const MockData = 0.4;
+/**
+ * 数据源
+ * @param percent 百分比
+ * @param color 颜色数组
+ * @param innerRadius 内环半径
+ * @param radius 外环半径
+ * @param statistic 内容
+ */
+export interface Data {
+  percent: number;
+  color: [string, string];
+  innerRadius: number;
+  radius: number;
+  statistic: {
+      title: {
+          content: string;
+          [key: string]: any;
+      };
+      content: boolean;
+  }
+}
--- src/ringProgress/runtime.tsx
+++ src/ringProgress/runtime.tsx
@@ -1,26 +1,40 @@
 import React, { useEffect, useState } from 'react';
 import { RingProgress } from '@ant-design/charts';
-import { Data, InputIds, MockData } from './constants';
+import { Data } from './constants';
 
-export default function ({ data, env, inputs, style }: RuntimeParams<Data>) {
-  const [percent, setPercent] = useState(0);
+export default function ({ data, inputs, env, title }) {
+  const [config, setConfig] = useState<Data>(data);
 
   useEffect(() => {
     if (env.runtime) {
-      inputs[InputIds.SetData]((val) => {
-        if (typeof val === 'number') {
-          setPercent(val);
+      inputs.style((ds: any) => {
+        setConfig({ ...config, ...ds });
+      });
+
+      inputs.percent((ds: number) => {
+        const dsNum = Number(ds);
+        if (isNaN(dsNum)) {
+          console.error(`${title}输入数据不合法！`);
+        } else {
+          setConfig({ ...config, percent: dsNum });
         }
+      });
+
+      const ids = ['statistic', 'tooltip'];
+      ids.forEach((id) => {
+        inputs[id]((ds: Object) => {
+          setConfig({ ...config, [id]: { ...ds } });
+        });
       });
     }
   }, []);
-
   return (
-    <RingProgress
-      {...style}
-      {...data.config}
-      percent={env.edit ? MockData : percent}
-      key={env.edit ? JSON.stringify(data.config) : undefined}
-    />
+    <>
+      {typeof config.percent === 'number' ? (
+        <RingProgress {...config} />
+      ) : (
+        <div style={{ display: 'flex', justifyContent: 'center' }}>暂无数据</div>
+      )}
+    </>
   );
 }
