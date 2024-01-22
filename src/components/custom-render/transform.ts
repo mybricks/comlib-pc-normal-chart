b90375596bf7228f995729427f9515d266b92b3a
zhuba-Ahhh <3477826311@qq.com>
2024-01-22 14:44:57 +0800

    remove package

--- src/components/custom-render/transform.ts
+++ src/components/custom-render/transform.ts
@@ -1,6 +1,3 @@
-import * as SchemaToTypes from '../../package/json-schema-to-typescript';
-import { getParamsType } from './constants';
-
 type FuncType = (code: string) => Function;
 
 const transform = (code: string) => {
@@ -37,19 +34,4 @@ const createElement: FuncType = (code) => {
   return eval(transformCode);
 };
 
-const genLibTypes = async (schema: Record<string, any>) => {
-  schema.title = 'Props';
-  const propTypes = await SchemaToTypes.compile(schema, '', {
-    bannerComment: '',
-    unknownAny: false,
-    format: false
-  }).then((ts: string) => {
-    return ts.replace('export ', '');
-  });
-  return `
-    ${propTypes}\n
-    ${getParamsType('Props')}
-  `;
-};
-
-export { createElement, genLibTypes };
+export { createElement };
