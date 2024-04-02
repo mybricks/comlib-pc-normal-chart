683db8d0e97cc73b00a90f48734eb18965b1a2e9
zhuba-Ahhh <3477826311@qq.com>
2024-04-02 12:42:41 +0800

    fix(charts): column showType switch

--- src/charts/editor.tsx
+++ src/charts/editor.tsx
@@ -107,6 +107,7 @@ export default {
               data.config = {
                 ...data.config,
                 isStack: true,
+                isGroup: false,
                 seriesField: 'type'
               };
             } else if (value === 'group') {
